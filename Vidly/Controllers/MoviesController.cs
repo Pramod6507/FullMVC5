1359d9723b89fae5b2687a05c2c3aec3f60bbcd5
Pramod Kumar <pramod6507@gmailcom>
2020-10-14 16:57:46 -0400

    Added custom route

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -31,5 +31,10 @@ namespace Vidly.Controllers
             }
             return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
         }
+
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
     }
 }
