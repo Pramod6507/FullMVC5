20fe9cb883dd0af1ce7390173875d1542059fabb
Pramod Kumar <pramod6507@gmailcom>
2020-10-14 19:58:30 -0400

    Migration to add Genre Coulmn to Movies table

--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -9,6 +9,8 @@ namespace Vidly.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public Genre Genre { get; set; }
+        public int GenreId { get; set; }
 
     }
 }
